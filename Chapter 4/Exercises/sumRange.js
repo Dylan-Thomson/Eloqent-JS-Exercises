ae9549d47a3b6c8cfe1bdb2d2b42cd0a335bb211
Dylan Thomson <djthomson88@gmail.com>
2018-07-18 15:18:18 -0400

    sumRange step
    TODO: Try to improve range() and then compare with book's solution

--- Chapter 4/Exercises/sumRange.js
+++ Chapter 4/Exercises/sumRange.js
@@ -2,9 +2,20 @@ function range(start, end, step) {
 	if(arguments.length < 3) {
 		step = 1;
 	}
+	if(step == 0) {
+		return NaN;
+	}
+
 	var arr = [];
-	for(var i = start; i <= end; i++) {
-		arr.push(i);
+	if(step < 0) {
+		for(var i = start; i >= end; i+=step) {
+			arr.push(i);
+		}
+	}
+	else {
+		for(var i = start; i <= end; i+=step) {
+			arr.push(i);
+		}
 	}
 	return arr;
 }
